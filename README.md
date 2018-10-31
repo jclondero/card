d63676d2f972414971519c9c78a27db8c8fcbeb7
Americo Neto <americoneto1@gmail.com>
2018-10-31 17:22:13 -0400

    feat(card): versão inicial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Americo Neto's Card
+
+Desenvolvedor web no Itaú Unibanco, pai, santista e coorganizador da comunidade AngularSP.
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/americoneto1/card/blob/master/LICENSE) 
+[![npm version](https://badge.fury.io/js/%40americoneto%2Fcard.svg)](https://www.npmjs.com/package/@americoneto/card)
+
+## Uso
+
+Via npx:
+
+```bash
+npx @americoneto/card
+```
+
+## Credits
+
+Forked from Tierney Cyren's https://github.com/bnb/bitandbang
+
+Badges from <https://badge.fury.io/for/js>
---
+++ bin/index.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+// Pull in our modules
+const boxen = require('boxen');
+const chalk = require('chalk');
+
+// Because we like console, and captains.log sounds cool
+const captains = console;
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'single',
+};
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Americo Neto /'),
+  handle: chalk.cyan('Dev'),
+  work: chalk.white('Desenvolvedor Web'),
+  twitter: chalk.cyan('https://twitter.com/americoneto1'),
+  github: chalk.cyan('https://github.com/americoneto1'),
+  linkedin: chalk.cyan('https://linkedin.com/in/americo-neto'),
+  npx: chalk.white('npx @americoneto/card'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelCard: chalk.white.bold('      Card:'),
+};
+
+// Actual strings we're going to output
+const newline = '\n';
+const heading = `${data.name} ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const carding = `${data.labelCard}  ${data.npx}`;
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading
+  + newline
+  + newline
+  + working
+  + newline
+  + twittering
+  + newline
+  + githubing
+  + newline
+  + linkedining
+  + newline
+  + newline
+  + newline
+  + carding;
+
+const cardMessage = chalk.green(boxen(output, options));
+captains.log(cardMessage);
